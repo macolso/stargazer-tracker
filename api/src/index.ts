6095ceb6246b57a1469ae0680ed1d83d6e0337b7
macolso <mackenzie.olson@fermyon.com>
2023-07-27 17:52:51 -0700

    started to modify index.ts
    Signed-off-by: macolso <mackenzie.olson@fermyon.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,18 @@
-import { HandleRequest, HttpRequest, HttpResponse} from "@fermyon/spin-sdk"
+import { HandleRequest, HttpRequest, HttpResponse, Sqlite} from "@fermyon/spin-sdk"
+
 
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
 
 export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
     let store = spinSdk.kv.openDefault()
-    let keys = store.getKeys()
-
-    // @ts-ignore
-    let result = [] 
-    keys.map(k=>{
-      if(k != "credentials")
-        result.push({date:k,value:decoder.decode(store.get(k))})
-    })
+    const conn = Sqlite.openDefault();
+    let data = conn.execute("SELECT * FROM gh_repo_stats",[]);
 
     return {
       status: 200,
         headers: { "content-type": "application/json" },
         // @ts-ignore
-        body: JSON.stringify(result)
+        body: JSON.stringify(data)
     }
 }
