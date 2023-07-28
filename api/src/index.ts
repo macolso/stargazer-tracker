61bf6b0648fcdaada417377013562ae0f72d919d
macolso <mackenzie.olson@fermyon.com>
2023-07-28 09:37:56 -0700

    typescript component working
    Signed-off-by: macolso <mackenzie.olson@fermyon.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,15 @@
 import { HandleRequest, HttpRequest, HttpResponse, Sqlite} from "@fermyon/spin-sdk"
 
-
 const encoder = new TextEncoder()
-const decoder = new TextDecoder()
 
 export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
-    let store = spinSdk.kv.openDefault()
-    const conn = Sqlite.openDefault();
-    let data = conn.execute("SELECT * FROM gh_repo_stats",[]);
+  const conn = Sqlite.openDefault();
+  let data = conn.execute("SELECT * FROM gh_repo_stats",[]);
 
-    return {
-      status: 200,
-        headers: { "content-type": "application/json" },
-        // @ts-ignore
-        body: JSON.stringify(data)
-    }
+  return {
+    status: 200,
+      headers: { "content-type": "application/json" },
+      // @ts-ignore
+      body: JSON.stringify(data)
+  }
 }
