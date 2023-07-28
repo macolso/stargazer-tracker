05468771162bc0803033b256e74787b166002bf7
macolso <mackenzie.olson@fermyon.com>
2023-07-28 15:04:12 -0700

    cleaned up api
    Signed-off-by: macolso <mackenzie.olson@fermyon.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,16 +4,21 @@ const encoder = new TextEncoder()
 
 export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
   const conn = Sqlite.openDefault();
-  let output = conn.execute("SELECT * FROM gh_repo_stats",[])
+  let output = conn.execute("SELECT * FROM gh_repo_stats",[]).rows
+  console.log(output.length)
 
-  // @ts-ignore
+   // @ts-ignore
   let result = [] 
-  output.rows.forEach(row=>{
-     // print(JSON.stringify(row))
-    result.push({date:row[0],stargazer_count:row[1],fork_count:row[2],open_issues_count:row[3]})
-  })
+  for (var row of output) {
+    console.log(row)
+    var values = JSON.stringify(row).split(",")
+    var date = values[0].substring(2).slice(0,-1)
+    var star_count = Number(values[1])
+    var fork_count = Number(values[2])
+    var issues_count = Number(values[3].slice(0,-1))
+    result.push({date:date,stargazer_count:star_count,fork_count:fork_count,open_issues_count:issues_count})
+  }
   
-
   return {
     status: 200,
       headers: { "content-type": "application/json" },
