1e8c17bd99534dec18c8ec998bec82cb9a7146f5
macolso <mackenzie.olson@fermyon.com>
2023-07-28 13:36:15 -0700

    messed around with api
    Signed-off-by: macolso <mackenzie.olson@fermyon.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,12 +4,20 @@ const encoder = new TextEncoder()
 
 export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
   const conn = Sqlite.openDefault();
-  let data = conn.execute("SELECT * FROM gh_repo_stats",[]);
+  let output = conn.execute("SELECT * FROM gh_repo_stats",[])
+
+  // @ts-ignore
+  let result = [] 
+  output.rows.forEach(row=>{
+     // print(JSON.stringify(row))
+    result.push({date:row[0],stargazer_count:row[1],fork_count:row[2],open_issues_count:row[3]})
+  })
+  
 
   return {
     status: 200,
       headers: { "content-type": "application/json" },
       // @ts-ignore
-      body: JSON.stringify(data)
+      body: JSON.stringify(result)
   }
 }
